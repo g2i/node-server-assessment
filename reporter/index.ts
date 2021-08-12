fd74d6be682f0d55e174b29b12ef3e10c55ec22e
Andrei Calazans <andreixoc@hotmail.com>
2021-08-12 11:34:50 -0300

    Reporter: extend log to include summary of tests

--- reporter/index.ts
+++ reporter/index.ts
@@ -1,5 +1,18 @@
 import { Reporter, DefaultReporter, Config, Context, AggregatedResult } from '@jest/reporters';
 
+// TODO - Bummer can't find types with PNP?  AggregatedResult["testResult"] 
+function buildSummary(results: AggregatedResult) {
+  let summary: string[] = [];
+  results.testResults.forEach(({ testResults }: any) => 
+    testResults.forEach((result: any) => 
+      summary.push(`${result.status === 'passed' ? '✓' : '✕'} ${result.fullName}`
+      )
+    )
+  );
+
+  return `START_SUMMARY\n${summary.join('\n')}\nEND_SUMMARY`;
+}
+
 export default class G2iReporter implements Reporter {
   #globalConfig: Config.GlobalConfig;
   #options: Record<string, unknown>;
@@ -10,7 +23,8 @@ export default class G2iReporter implements Reporter {
   }
 
   onRunStart(): void {}
-  onRunComplete(contexts: Set<Context>, results: AggregatedResult) {
+  onRunComplete(_contexts: Set<Context>, results: AggregatedResult) {
+    console.log(buildSummary(results));
     console.log(`result=${results.numPassedTests / results.numTotalTests}`)
   }
   getLastError(): void {}
